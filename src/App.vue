a8c072e4541a94cb9214ad96b37531b205b660e9
artyom021 <achuluhadze@braintch.com>
2023-07-28 20:31:25 +0300

    PrimeVue added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="app" id="app">
-
+    <PageHeader />
+    <router-view v-slot="{ Component }">
+      <suspense>
+        <div>
+          <component :is="Component" :key="$route.path"></component>
+        </div>
+      </suspense>
+    </router-view>
+    <PageFooter />
   </div>
 </template>
 
 <script lang="ts" setup>
-
 
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 
 import i18nPlugin from "@/plugins/i18nPlugin";
-
+import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -15,4 +16,6 @@ createApp(App)
   .use(store)
   .use(router)
   .use(i18nPlugin)
+  .use(PrimeVue)
+  .use(ToastService)
   .mount("#app");
