7306085ecff4e8e185c2ef446dfc00b6fe1e7ef8
artyom021 <achuluhadze@braintch.com>
2023-07-28 20:12:29 +0300

    Rendunant code removed

--- src/i18n/en/langMenu.ts
+++
@@ -1,6 +0,0 @@
-import { Lang } from "@/types/lang";
-
-export default <Lang>{
-  logout: "Logout",
-  myProfile: "My Profile",
-};
--- src/i18n/en/langStatus.ts
+++
@@ -1,9 +0,0 @@
-import { Lang } from "@/types/lang";
-
-export default <Lang>{
-  inPlatform: "In-Platform",
-  linked: "Linked",
-  removing: "Removing",
-  uploading: "Uploading",
-  error: "Error",
-};
